62ccf2924c0a24059c115725598ceca22c74c4c4
rajesh-oc <rajesh@webkorps.com>
2023-10-16 08:41:33 -0400

    enhance navbar

--- app/javascript/bundles/Navbar/components/Navbar.jsx
+++ app/javascript/bundles/Navbar/components/Navbar.jsx
@@ -4,12 +4,17 @@ import style from "./Navbar.module.css"
 const Navbar = (props) => {
   return(
     <div className={style.navbar}>
-      <span className={style.item}>
-      <i class="fa-solid fa-dice-d20" />
-      </span>
-      <span className={`${style.item} ${style.first}`}>
-      <i class="fa-solid fa-gear" />
-      </span >
+      <div className={style.holder}>
+        <span className={style.item}>
+          <i class="fa-solid fa-dice-d20" />
+        </span>
+      </div>
+
+      <div className={style.holder}>
+        <span className={`${style.item} ${style.first}`}>
+          <i class="fa-solid fa-gear" />
+        </span >
+      </div>
     </div>
   )
 }

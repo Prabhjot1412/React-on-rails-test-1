5ac713a9891df8f3d199a1c715146c915e351f67
rajesh-oc <rajesh@webkorps.com>
2023-10-17 09:07:09 -0400

    add expiry

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,17 @@ class ApplicationController < ActionController::Base
 
   def current_user
     return if session[:user_token].nil?
+    return if expired?
 
     user_id = JwtService.decode(session[:user_token])[:user_id]
     User.find(user_id)
+  end
+
+  def expired?
+    generated_at = JwtService.decode(session[:user_token])[:generated_at].to_datetime
+    return false if generated_at > 6.hours_ago
+
+    reset_session
+    true
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,10 +7,18 @@ module ApplicationHelper
   end
 
   def current_user
-    nil
     return if session[:user_token].nil?
+    return if expired?
 
     user_id = JwtService.decode(session[:user_token])[:user_id]
     User.find(user_id)
+  end
+
+  def expired?
+    generated_at = JwtService.decode(session[:user_token])[:generated_at].to_datetime
+    return false if generated_at > 6.hours.ago
+
+    reset_session
+    true
   end
 end
